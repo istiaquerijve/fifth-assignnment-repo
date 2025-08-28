c99a30edb9ed7d894741f8455757857cd664dcdb
istiaquerijve <istiaquerijve@gmail.com>
2025-08-28 08:54:36 +0600

    eighth commit

--- js/index.js
+++ js/index.js
@@ -1,1 +1,36 @@
-console.log('ok');
+ document.getElementsByClassName("heart-btn").addEventListener("click", function (e) {
+      e.preventDefault()
+   //  console.log('heart ok');
+    const heart = parseInt(document.getElementById("heart").value)
+    const navHeart = heart.value + 1;
+    document.getElementById("heart").value = navHeart
+     
+})
+
+document.getElementById("call-btn").addEventListener("click", function () {
+    const call = document.getElementById("call").innerText
+    const olCall = call - 20;
+     document.getElementById("call").innerText = olCall
+})
+
+
+
+// ---------------------------------------------------------------
+
+
+function copyText() {
+      
+      const textElement = document.getElementById('textToCopy');
+
+     
+      const text = textElement.textContent;
+
+     
+      navigator.clipboard.writeText(text)
+        .then(() => {
+          alert("টেক্সট কপি হয়েছে: " + text);
+        })
+        .catch(err => {
+          alert("কপি করা যায়নি: " + err);
+        });
+    }
